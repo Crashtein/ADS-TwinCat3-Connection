c4bb8ed1ff9eedb41aebe297e5ff257b6e231fef
Crashtein <51822683+Crashtein@users.noreply.github.com>
2021-07-03 20:17:46 +0200

    First alpha release
    App should be able to connect and read values into UI Text Box

---
+++ ADSTwinCat3/ADSConnection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TwinCAT.Ads;
+
+namespace ADSTwinCat3
+{
+    class ADSConnection
+    {
+        TcAdsClient tcAds = new TcAdsClient();
+        private string ADSAddress = "127.0.0.1.1.1";
+        private int ADSPort = 851;
+        public string Connect()
+        {
+            try
+            {
+                tcAds.Connect(ADSAddress, ADSPort);
+            }catch(Exception ex)
+            {
+                if (ex != null)
+                {
+                    return ex.ToString();
+                }
+            }
+            return "Connected";
+        }
+        public string ReadVariables()
+        {
+            // creates a stream with a length of 4 byte 
+            AdsStream ds = new AdsStream(4);
+            BinaryReader br = new BinaryReader(ds);
+
+            // reads a DINT from PLC
+            try
+            {
+                tcAds.Read(0x4020, 0, ds);
+            }catch(Exception ex)
+            {
+                if (ex != null)
+                {
+                    return ex.ToString();
+                }
+            }
+            
+            ds.Position = 0;
+            return br.ReadInt32().ToString();
+        }
+        public void WriteVariables()
+        {
+            // creates a stream with a length of 4 byte
+            AdsStream ds = new AdsStream(4);
+            BinaryWriter bw = new BinaryWriter(ds);
+
+            ds.Position = 0;
+            bw.Write((int)100);
+
+            // writes a DINT to PLC
+            tcAds.Write(0x4020, 0, ds);
+        }
+        public void Dispose()
+        {
+            if (tcAds != null)
+            {
+                tcAds.Dispose();
+            }
+        }
+        ~ADSConnection()
+        {
+            Dispose();
+        }
+    }
+}
---
+++ ADSTwinCat3/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace ADSTwinCat3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectBox = new System.Windows.Forms.TextBox();
+            this.ReadButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ReadBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ConnectBox
+            // 
+            this.ConnectBox.Location = new System.Drawing.Point(14, 14);
+            this.ConnectBox.Name = "ConnectBox";
+            this.ConnectBox.ReadOnly = true;
+            this.ConnectBox.Size = new System.Drawing.Size(774, 20);
+            this.ConnectBox.TabIndex = 0;
+            // 
+            // ReadButton
+            // 
+            this.ReadButton.Location = new System.Drawing.Point(14, 113);
+            this.ReadButton.Name = "ReadButton";
+            this.ReadButton.Size = new System.Drawing.Size(156, 41);
+            this.ReadButton.TabIndex = 1;
+            this.ReadButton.Text = "Read";
+            this.ReadButton.UseVisualStyleBackColor = true;
+            this.ReadButton.Click += new System.EventHandler(this.ReadButton_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(13, 40);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(157, 41);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ReadBox
+            // 
+            this.ReadBox.Enabled = false;
+            this.ReadBox.Location = new System.Drawing.Point(14, 87);
+            this.ReadBox.Name = "ReadBox";
+            this.ReadBox.ReadOnly = true;
+            this.ReadBox.Size = new System.Drawing.Size(774, 20);
+            this.ReadBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReadBox);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.ReadButton);
+            this.Controls.Add(this.ConnectBox);
+            this.Name = "Form1";
+            this.Text = "ADS Connect TwinCat 3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ConnectBox;
+        private System.Windows.Forms.Button ReadButton;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox ReadBox;
+    }
+}
+
